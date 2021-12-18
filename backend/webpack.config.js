1d025517c878b0468a0412a5bebe6adbcd3a0bb9
goodingc <callum.gooding11@gmail.com>
2021-12-18 16:16:39 +0000

    18/12 a

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,27 +1,27 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'build'),
-        libraryTarget: "umd",
-        // globalObject: "this"
-    },
-    target: 'node',
-    optimization: {
-        minimize: false, // enabling this reduces file size and readability
-    },
+  entry: "./index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+    libraryTarget: "umd",
+    // globalObject: "this"
+  },
+  target: "node",
+  optimization: {
+    minimize: false, // enabling this reduces file size and readability
+  },
 };
