6c8d74396edd8e7dd7df710f5d7414602a7d1c22
goodingc <callum.gooding11@gmail.com>
2021-12-18 18:25:15 +0000

    18/12 i

--- frontend2/plugins/comms.client.ts
+++ frontend2/plugins/comms.client.ts
@@ -1,25 +1,25 @@
-import Vue from "vue";
+import Vue from 'vue'
 
-declare module "vue/types/vue" {
+declare module 'vue/types/vue' {
   interface Vue {
-    $call(method: string, args: any): Promise<any>;
+    $call(method: string, args: any): Promise<any>
   }
 }
 
 Vue.prototype.$call = async (method: string, args: any): Promise<any> => {
-  const commsHost = location.hostname.includes("www") ?
-    "https://p3ej2v7m2k.execute-api.eu-west-2.amazonaws.com/default/spotify-playlist-splitter-backend" :
-    `http://${location.hostname}:3001`;
+  const commsHost = location.hostname.includes('goodingc.github.io')
+    ? 'https://p3ej2v7m2k.execute-api.eu-west-2.amazonaws.com/default/spotify-playlist-splitter-backend'
+    : `http://${location.hostname}:3001`
   const request = await fetch(commsHost, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json',
     },
-    mode: "cors",
+    mode: 'cors',
     body: JSON.stringify({
       method,
-      args
-    })
-  });
-  return request.json();
-};
+      args,
+    }),
+  })
+  return request.json()
+}
