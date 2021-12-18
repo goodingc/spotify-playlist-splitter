ff6f4267d301d449e59e7bbf55000f50690419e7
goodingc <callum.gooding11@gmail.com>
2021-12-18 16:33:13 +0000

    18/12 c

--- frontend2/components/login-page.vue
+++ frontend2/components/login-page.vue
@@ -10,6 +10,7 @@
 
 <script lang="ts">
 import Vue from "vue";
+import * as process from "process";
 
 export default Vue.extend({
   name: "login-page",
@@ -18,7 +19,7 @@ export default Vue.extend({
       const state = Math.random().toString(36).substring(2, 18);
       window.location.href = `https://accounts.spotify.com/authorize?` +
         `client_id=${"ad1e08d3838e4a518096eddf000e1358"}&` +
-        `response_type=code&redirect_uri=http://${location.host}/redirect&` +
+        `response_type=code&redirect_uri=${process.env.PUBLIC_PATH || 'http://' + location.host}/redirect&` +
         `scope=${"user-read-private user-read-email playlist-read-private playlist-read-collaborative"}&` +
         `state=${state}`;
     }
--- frontend2/nuxt.config.js
+++ frontend2/nuxt.config.js
@@ -61,6 +61,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    publicPath: process.env.PUBLIC_PATH || '/',
+    publicPath: process.env.PUBLIC_PATH,
   },
 }
