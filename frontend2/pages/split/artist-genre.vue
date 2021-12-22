5952abeb8677895c748ace30af8565bcaa9bce16
goodingc <callum.gooding11@gmail.com>
2021-12-22 18:07:31 +0000

    22/12 b

--- frontend2/pages/split/artist-genre.vue
+++ frontend2/pages/split/artist-genre.vue
@@ -8,25 +8,25 @@ import { spotifyStore } from "~/utils/store-accessor";
 
 export default Vue.extend({
   async created() {
-    const details = await spotifyStore.getPlaylistDetails(this.$route.query.playlist as string);
-    const trackGenres: [string, string[]][] = await Promise.all(details.tracks.items.map(async track => {
-      if (track.track.type === "track") {
-        return [track.track.id, (await spotifyStore.getArtist(track.track.artists[0].id)).genres];
-      }
-      return [track.track.id, []];
-    }));
-
-    const genreIndices = new Map<string, number>();
-    let nextGenreIndex = 0;
-    const genreData = trackGenres.map(([_, genres]) =>
-      genres.map(genre => {
-        if (!genreIndices.has(genre)) {
-          genreIndices.set(genre, nextGenreIndex++);
-        }
-        return genreIndices.get(genre);
-      }));
-    console.log(trackGenres);
-    console.log(genreData);
+    // const details = await spotifyStore.getPlaylistDetails(this.$route.query.playlist as string);
+    // const trackGenres: [string, string[]][] = await Promise.all(details.tracks.items.map(async track => {
+    //   if (track.track.type === "track") {
+    //     return [track.track.id, (await spotifyStore.getArtist(track.track.artists[0].id)).genres];
+    //   }
+    //   return [track.track.id, []];
+    // }));
+    //
+    // const genreIndices = new Map<string, number>();
+    // let nextGenreIndex = 0;
+    // const genreData = trackGenres.map(([_, genres]) =>
+    //   genres.map(genre => {
+    //     if (!genreIndices.has(genre)) {
+    //       genreIndices.set(genre, nextGenreIndex++);
+    //     }
+    //     return genreIndices.get(genre);
+    //   }));
+    // console.log(trackGenres);
+    // console.log(genreData);
   }
 });
 </script>
